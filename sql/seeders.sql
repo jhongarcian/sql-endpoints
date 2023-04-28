618b7ced4f6da1026e60d0ea34b0f7df1e0af0c7
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-27 19:51:50 -0500

    add sql file that contains values

---
+++ sql/seeders.sql
@@ -0,0 +1,22 @@
+INSERT INTO "Students" ("firstName", "lastName", "city", "createdAt", "updatedAt") VALUES
+
+('Christopher', 'Bond','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Robert', 'Grimes','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Kelly', 'Petersen','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Joseph', 'Caldwell','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Frank', 'Williams','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Carrie', 'Simpson','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('April', 'Weeks','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Ronald', 'Davis','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Corey', 'Rivera','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Felicia', 'White','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Robert', 'Casey','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Adam', 'Scott','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Darrell', 'Moore','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Joseph', 'Kelly','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Jordan', 'Carlson','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Tammy', 'Jones','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('James', 'Moore','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Thomas', 'Malone','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Pamela', 'Russell','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3)),
+('Craig', 'Mckenzie','Fargo',CURRENT_TIMESTAMP(3),CURRENT_TIMESTAMP(3));
