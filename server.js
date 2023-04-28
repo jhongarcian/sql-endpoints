e7be14fdc97dfc0a0e9f1bb9ee0392be8ffbb870
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-27 19:44:30 -0500

    add endpoint for students connected with database

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+const { json } = require("body-parser");
 const express = require("express");
+const pgp = require('pg-promise')();
 
 const PORT = 8080;
 
 const server = express();
+server.use(json())
+
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'sept2022',
+    user: 'jhongarcian',
+    password: '',
+    allowExitOnIdle: true
+}
+
+const db =pgp(cn);
+
+server.get('/students', async (req, res) => {
+    const students = await getStudents();
+    res.json(students)
+})
+
+async function getStudents() {
+    const students = await db.any('SELECT * FROM students', [false]);
+    return students;
+}
 
 // Health endpoint 
 server.get('/heartbeat', (req, res) => {
